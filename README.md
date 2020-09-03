4254d0ff95e25da8c48f28cf304fece9731f32cf
hello~git <971341273@qq.com>
2020-09-03 16:16:13 +0800

    fixing bug

--- README.md
+++ README.md
@@ -1,11 +1,25 @@
 使用方法：
     在example-config中配置好图片关键字
-    python google-spy.py 开始运行爬虫
+    在当前目录下,cmd中输入指令setup.py运行程序
     下载好的图片会放在当前目录下
+    使用google爬取图片，需要代理
+    百度默认爬取前100个json数据包,以后会开放页数接口
 
-配置文件写法：数量任意
-    {"key":关键字}
-    {"key":关键字}
+配置文件写法：
+    1.同时使用百度与google
+        {'key':'关键字'}
+    2.只使用百度
+        {'key':'关键字','spider':'baidu'}
+    3.只是用google
+        {'key':'关键字','spider':'google'}
+
+    例子
+        {"key":"apple","spider":"baidu"}
+        {"key":"bear","spider":"google"}
+        {"key":"nab"}
+        使用百度爬取关键字为apple的图片
+        使用google爬取关键字为bear的图片
+        使用百度与谷歌同时爬取nba的图片
 
 配置环境：
     pip install selenium 
@@ -16,7 +30,7 @@
     下载完成后，放在chrome根目录下，并加入环境变量
 
 待完善
-    加入百度图片
-    自定义下载地址
+    给百度的下载器加入多线程
     自动配置环境
-    '''
+    开放百度页数接口
+    开放配置文件接口
--- baiduspy.py
+++ baiduspy.py
@@ -1,3 +1,6 @@
+from http.client import responses
+from os import times
+from threading import TIMEOUT_MAX
 import requests
 from bs4 import BeautifulSoup
 import urllib
@@ -6,6 +9,12 @@ import json
 import urllib.request
 import sys
 import os
+import time
+import re
+import threading
+from queue import Queue
+import socket
+socket.setdefaulttimeout(5)
 class Crawljson():
     def __init__(self,key,page=100):
         self.key =key
@@ -23,29 +32,63 @@ class Crawljson():
     def run(self):
         if self.page >0:
             for i in range(1,self.page+1):
-                self.geturl(i)
+                if not self.geturl(i):
+                    print("无法获取多余的图像地址")
+                    break
         self.imagelist =list(set(self.imagelist))
         self.totallink =len(self.imagelist)
         print('finding img_link:',self.totallink)
         if len(self.imagelist):
             for i in self.imagelist:
                 self.Downloader(i)
+        else:
+            print('no page')
+        
+        '''
+            add thread
+        '''
+        # Downloadthreadname =['Download1','Download2','Download3','Download4','Download5']
+        # linkqueue =Queue()
+        # junk_Downloadthreadnamelist=[]
+
+        # for i in self.imagelist:
+        #     linkqueue.put(i)
+        
+        # while not linkqueue.empty():
+        #     for i in range(0,5):
+        #         try:
+        #             link =linkqueue.get(False)
+        #             thread =threading.Thread(target=self.Downloader,args=(link,))
+        #             thread.start()
+        #             #thread.join()
+        #         except Exception as e:
+        #             break
     
+  
+
     def geturl(self,page):
         print('Crawling page:',page)
         page =(page-1)*30
         queryWord =urllib.parse.urlencode({'queryWord':self.key}).upper().replace('QUERYWORD','queryWord')
         word =urllib.parse.urlencode({'word':self.key}).upper().replace('WORD','word')
         url ='https://image.baidu.com/search/acjson?tn=resultjson_com&ipn=rj&is=&fp=result&'+queryWord+'&cl=2&lm=-1&ie=utf-8&oe=utf-8&adpicid=&st=-1&z=&ic=&hd=&latest=&copyright=&'+word+'&s=&se=&tab=&width=&height=&face=0&istype=2&qc=&nc=1&fr=&expermode=&force=&pn='+str(page)+'&rn=30'
+        print('linking:',url)
         response =self.session.get(url,headers=self.headers)
-        jsondata =json.loads(response.text)
+        time.sleep(1)
         try:
-            data =jsondata['data']
-            if len(data):
-                for i in data:
-                    self.imagelist.append(i['thumbURL'])
+            tempimagelist =re.findall(r'"thumbURL":"\S+","middleURL"',response.text)
+            print("downloadurl num:",len(tempimagelist))
+            if len(tempimagelist) ==0:
+                return False
+            if len(tempimagelist):
+                for i in tempimagelist:
+                    downloadurl =i.replace('"thumbURL":"','').replace('","middleURL"','')
+                    print("fining",downloadurl)
+                    self.imagelist.append(downloadurl)
+            print('success linking',url)
         except  Exception as e:
-            pass
+            print(e)
+        return True
     
     def Downloader(self,url):
         path =self.path+'/'+self.key+'_'+str(self.count)+'.jpg'
@@ -74,7 +117,7 @@ class Crawljson():
 
 def main(key):
     if key !=None:
-        Crawl =Crawljson(key,'2')
+        Crawl =Crawljson(key,'100')
         Crawl.run()
     else:
         print('baiduspy:缺少关键字')
